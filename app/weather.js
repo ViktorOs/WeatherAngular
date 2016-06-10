2f97a8da1e8ffc91995c8ef52cabd6b46cc55172
ViktorOs <viktorosa@gmail.com>
2016-06-10 15:52:40 +0300

    geolocation added

--- app/weather.js
+++ app/weather.js
@@ -1,18 +1,29 @@
 angular.module('weatherApp', [])
-.controller('WeatherController', function ($scope, $http) {
-    if (navigator.geolocation) {
-        geolocation.getCurrentPosition().then(function(data){
-          $scope.coords = {lat:data.coords.latitude, long:data.coords.longitude};
+.controller('WeatherController', function ($scope, $http, $window) {
+
+    if ($window.navigator.geolocation) {
+        $window.navigator.geolocation.getCurrentPosition(function(position){
+            
+            $scope.$apply(function(){
+                $scope.coords = {
+                    lat:position.coords.latitude, 
+                    lon:position.coords.longitude
+                }; 
+                var url = "http://api.openweathermap.org/data/2.5/weather?lat=" + $scope.coords.lat + "&lon=" + $scope.coords.lon + "&APPID=a73e673d671afa8ac0a1a20cbeca3ed2"; 
+                $scope.sendPost(url)
+            })
+            
+        },function(error){
+            console.log(error.message)
         });
-    }
-    
-    console.log(navigator.geolocation)
-    console.log($scope.coords)
+    }    
 
-    $scope.sendPost = function() {
-
-        /*var url = "http://api.openweathermap.org/data/2.5/forecast/city?id=703448&APPID=a73e673d671afa8ac0a1a20cbeca3ed2"*/
-        var url = "http://api.openweathermap.org/data/2.5/weather?q=" + $scope.cityName + "&APPID=a73e673d671afa8ac0a1a20cbeca3ed2";
+    $scope.sendPost = function(url) {
+        if (url) {
+            url = url;
+        }else{
+            var url = "http://api.openweathermap.org/data/2.5/weather?q=" + $scope.cityName + "&APPID=a73e673d671afa8ac0a1a20cbeca3ed2";
+        }
        
         $http.get(url).success(function(data, status) {
 
@@ -27,8 +38,7 @@ angular.module('weatherApp', [])
             $scope.sunset = data.sys.sunset;   
             $scope.geo_coords_lat = data.coord.lat;
             $scope.geo_coords_lon = data.coord.lon;
-            $scope.currentCity = $scope.cityName;
+            $scope.currentCity = data.name || $scope.cityName;
         })
-
     }                   
 })
